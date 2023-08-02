8dd57e1c16fb49dc6181e86c985919238af36718
Rejaul42 <rejaulkarimraju91@gmail.com>
2023-08-02 05:40:17 -0600

    fineshed

--- signal.js
+++ signal.js
@@ -1,12 +1,12 @@
 function signal(value){
     if(value === 'red'){
-        return 'you maybe in the danger';
+        return 'You maybe in the danger';
     }
     else if(value === 'yellow'){
         return 'You should stop'
     }
     else if(value === 'green'){
-        return 'you should cross the road'
+        return 'You should cross the road'
     }
     else{
         return 'No activities'
